c4f1b6026528b357ddd577d435371363bb5d8cab
Alexander Rao <alexhrao@gmail.com>
2019-12-18 03:06:28 +0530

    Updates

---
+++ precache-manifest.7cd174d6f42feb6b14266a57a0f9fabb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add44ffdbcc6d9fcadb219591e36bc4a",
+    "url": "/solitaire/index.html"
+  },
+  {
+    "revision": "5a83a1898a5509e4989c",
+    "url": "/solitaire/static/css/main.0ad66599.chunk.css"
+  },
+  {
+    "revision": "3fa756ecd1492be3f1bb",
+    "url": "/solitaire/static/js/2.f6fbaf7c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/solitaire/static/js/2.f6fbaf7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a83a1898a5509e4989c",
+    "url": "/solitaire/static/js/main.69930618.chunk.js"
+  },
+  {
+    "revision": "4aec2c863fa0a60430db",
+    "url": "/solitaire/static/js/runtime-main.398ef90c.js"
+  }
+]);
